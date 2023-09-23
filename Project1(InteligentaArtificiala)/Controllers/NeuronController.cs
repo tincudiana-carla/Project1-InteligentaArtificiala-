4d88907f07a98b847410cd642bff7ef486640c7f
tincudiana-carla <carla_diana@Yahoo.com>
2023-09-23 15:43:41 +0300

    Calculating GIN (Global Inhibition Network)

--- Project1(InteligentaArtificiala)/Controllers/NeuronController.cs
+++ Project1(InteligentaArtificiala)/Controllers/NeuronController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Project1_InteligentaArtificiala_.GeneratingGin;
 using Project1_InteligentaArtificiala_.Models;
 
 namespace Project1_InteligentaArtificiala_.Controllers
@@ -36,6 +37,28 @@ namespace Project1_InteligentaArtificiala_.Controllers
             }
 
             return RedirectToAction("Index");
+        }
+
+        public IActionResult CalculateGIN(string operation)
+        {
+            double ginResult = 0.0;
+            switch (operation)
+            {
+                case "sum":
+                    ginResult = FunctionGIN.SUM(NeuronViewModel.Neurons);
+                    break;
+                case "product":
+                    ginResult = FunctionGIN.PROD(NeuronViewModel.Neurons);
+                    break;
+                case "max":
+                    ginResult = FunctionGIN.MAX(NeuronViewModel.Neurons);
+                    break;
+                case "min":
+                    ginResult = FunctionGIN.MIN(NeuronViewModel.Neurons);
+                    break;
+            }
+            ViewBag.GINResult = ginResult;
+            return View("Index", NeuronViewModel.Neurons);
         }
     }
 }
---
+++ Project1(InteligentaArtificiala)/GeneratingGin/FunctionGIN.cs
@@ -0,0 +1,56 @@
+﻿using Project1_InteligentaArtificiala_.Models;
+
+namespace Project1_InteligentaArtificiala_.GeneratingGin
+{
+    public class FunctionGIN
+    {
+        public static double SUM(List<Neuron> neurons)
+        {
+            double sum = 0.0;
+            foreach (var neuron in neurons)
+            {
+                sum += neuron.x * neuron.w;
+            }
+            return sum;
+        }
+
+        public static double PROD(List<Neuron> neurons)
+        {
+            double prod = 1.0; 
+            foreach (var neuron in neurons)
+            {
+                prod *= neuron.x * neuron.w;
+            }
+            return prod;
+        }
+
+        public static double MAX(List<Neuron> neurons)
+        {
+            double maxValue = double.MinValue;
+            foreach (var neuron in neurons)
+            {
+                double value = neuron.x * neuron.w;
+                if (value > maxValue)
+                {
+                    maxValue = value;
+                }
+            }
+            return maxValue;
+        }
+
+        public static double MIN(List<Neuron> neurons)
+        {
+            double minValue = double.MaxValue;
+            foreach (var neuron in neurons)
+            {
+                double value = neuron.x * neuron.w;
+                if (value < minValue)
+                {
+                    minValue = value;
+                }
+            }
+            return minValue;
+        }
+
+    }
+}
